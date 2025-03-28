a7505e2678348ed10ba2b193e430a90f0fe990ec
Flaya <delphine.jozeau@gmail.com>
2025-03-28 12:35:25 +0100

    Etape2 OK Etape 3 preque

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // A toi de jouer pour cette partie :-) Happy coding !
 
 const APIgeocoding = "https://nominatim.openstreetmap.org"; // /search?q=Lyon&format=json&addressdetails=1&limit=1
-const APImeteo = 0;
+const APImeteo = "https://api.open-meteo.com"; // /v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,precipitation,relative_humidity_2m
 
 
 const buttonCity = document.querySelector("#buttonCity");
@@ -13,35 +13,66 @@ const detailsData =  document.querySelector("#details");
 
 //Coordonnées de la ville avec Nominatim
 
-async function getCityCoordonnees() {
+async function fetchCoordinates() {
     let cityInput = document.querySelector("#cityInput").value;
+    //console.log(cityInput);
 
     try {
       const response = await fetch(
-        `${APIgeocoding}search?q=${cityInput}&format=json&addressdetails=1&limit=1 `     
+        `${APIgeocoding}/search?q=${cityInput}&format=json&addressdetails=1&limit=1 `     
       );
+    console.log(response);
 
-      const data = await response.json();
-      let latitude = data[0].lat;
-      let longitude = data[0].lon;
 
-      if (data.error) {
-        cityData.innerHTML = "Ville Inconnue";
+      const dataNominatim = await response.json();
+      let latitude = dataNominatim[0].lat;
+      let longitude = dataNominatim[0].lon;
+
+      if (dataNominatim.length === 0) {
+        cityData.innerHTML = "Ville non trouvée";
         gpsData.innerHTML = "Coordonnées introuvables"
-      } else {
+      } 
+      
         cityData.innerHTML = cityInput;
         gpsData.innerHTML = `Coordonnées GPS : ${latitude}, ${longitude}`
-      }
+        fetchWeather(latitude, longitude)
+      
 
     } catch (error) {
       console.error(error);
+      cityData.innerHTML = "Une erreur s'est produite."
     }
   }
+
+  async function fetchWeather(latitude, longitude){
+
+    try {
+        const response = await fetch(
+          `${APImeteo}/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,precipitation,relative_humidity_2m`
+        );
+        
+        const data = await response.json();
+        let temperature = data.current_units.temperature_2m;
+
+        if (data.length === 0) {
+        temperatureData.innerHTML = " Température inconnue ";
+        } 
+        
+        temperatureData = " "; 
+        temperatureData.innerHTML = `${temperature}°C`
+        
+
+      } catch (error) {
+        console.error(error);
+        temperatureData.innerHTML = "erreur"
+      }
+    }
+
 
 //executer
 
 buttonCity.addEventListener("click", function ()  {
-getCityCoordonnees()
+    fetchCoordinates();
   });
 
 
