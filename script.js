6a07ddfd18b77a5468eca2cf724a975849e44c50
Flaya <delphine.jozeau@gmail.com>
2025-03-28 18:20:15 +0100

    pays

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const cityData = document.querySelector("#city");
 const gpsData = document.querySelector("#gps");
 const temperatureData = document.querySelector("#temperature");
 const details = document.querySelector("#details");
+const countryData = document.querySelector("#pays");
 
 //Coordonnées de la ville avec Nominatim
 
@@ -19,11 +20,13 @@ async function fetchCoordinates() {
         const response = await fetch(
             `${APIgeocoding}/search?q=${cityInput}&format=json&addressdetails=1&limit=1 `
         );
-        //console.log(response);
+        console.log(response);
 
         const dataNominatim = await response.json();
         let latitude = dataNominatim[0].lat;
         let longitude = dataNominatim[0].lon;
+        let city = dataNominatim[0].name;
+        let country = dataNominatim[0].address.country;
 
         if (dataNominatim.length === 0) {
             cityData.innerHTML = "Ville non trouvée";
@@ -34,15 +37,17 @@ async function fetchCoordinates() {
             cityData.innerHTML = "Veuillez taper une ville"
         }
 
-        cityData.innerHTML = cityInput;
-        gpsData.innerHTML = `Coordonnées GPS : ${latitude}, ${longitude}`
+        cityData.innerHTML = city;
+        gpsData.innerHTML = `Coordonnées GPS : ${latitude}, ${longitude}`;
+        countryData.innerHTML =`Pays : ${country}`;
         fetchWeather(latitude, longitude)
-
-
+        
     } catch (error) {
         console.error(error);
         cityData.innerHTML = "Une erreur s'est produite."
     }
+
+    
 }
 
 async function fetchWeather(latitude, longitude) {
@@ -64,9 +69,9 @@ async function fetchWeather(latitude, longitude) {
         else {
             temperatureData.innerHTML = `${temperature}°C`;
             details.innerHTML = "température actuelle"
-if(temperature <= 10){temperatureData.style.color = "blue"}
-else if (temperature > 25){temperatureData.style.color = "red"}
-else{temperatureData.style.color = "purple"}
+            if (temperature <= 10) { temperatureData.style.color = "blue" }
+            else if (temperature > 25) { temperatureData.style.color = "red" }
+            else { temperatureData.style.color = "purple" }
         };
 
     } catch (error) {
